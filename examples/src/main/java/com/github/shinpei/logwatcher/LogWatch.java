8d5c8368a340f7a7cbe717285f857f6bc0b0caaa
shinpei <shinpei.nakata@gmail.com>
2014-03-05 00:01:40 +0900

    update example

--- examples/src/main/java/com/github/shinpei/logwatcher/LogWatch.java
+++ examples/src/main/java/com/github/shinpei/logwatcher/LogWatch.java
@@ -1,9 +1,24 @@
 package com.github.shinpei.logwatcher;
 
 import com.github.shinpei.jinotify.Jinotify;
+import com.github.shinpei.jinotify.JinotifyListener;
 
 public class LogWatch {
     static public void main(String[] args) {
-        
+
+        Jinotify jinotify = new Jinotify();
+        class MyListener extends JinotifyListener {
+            @Override
+            public void onModify() {
+                System.out.println("hi");
+            }
+        }
+        try {
+            jinotify.addWatch("/var/log/system.log", Jinotify.MODIFY, new MyListener());
+
+        } catch (Exception e) {
+
+        }
+        jinotify.closeNotifier();
     }
 }
